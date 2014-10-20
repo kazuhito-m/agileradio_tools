6a269fe8fc503b6bf8e1b0dbf67915c5bc859c82
kazuhito_m <kazuhito.sumpic@gmail.com>
2014-10-20 17:35:03 +0900

    fixed for my environment.

--- all_archive.sh
+++ all_archive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # agileradio* directory archiveing.
-for i in  $(find ./ite* -type d -name 'agileradio_ite0*'| grep -v 'interchange') ; do 7za a $i.7z $i ; done
+for i in  $(find ./ite* -type d -name 'agileradio_ite*'| grep -v 'interchange') ; do 7za a $i.7z $i ; done
 
 # fixed directory archiveing.
 for i in $( find ./ite* -name 'fixed') ; do 7za a $i.7z $i ; done
--- radio_enc.sh
+++ radio_enc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # agileradioのmp3エンコード＆タグ情報埋め込みスクリプト
 # Usage. [this script] [source WAV file]
@@ -7,14 +7,14 @@
 # 定数
 
 # mp3のサイズの限界		
-MAX_MP3_SIZE=`expr 20 \* 1024 \* 1024`
+MAX_MP3_SIZE=`expr 24 \* 1024 \* 1024`
 # 結果ファイル
 RESULT_FILE=${1%.*}.mp3
 
 # 処理
 
 # lameに「mp3の圧縮レベル」を渡してMAXを切るまで変換し続ける
-for level in $(seq 5 10); do
+for level in $(seq 3 10); do
 	echo "Try encoding Level ${level}"
 	# 変換
 	lame -V ${level} ${1}
@@ -37,7 +37,7 @@ echo "Mp3 encoding done."
 # Tagの書き込み (yearが効かないなど、いくつか不思議な項目がある)
 # 定義はこちらを参照。http://wikiwiki.jp/qmp/?Plugins%2FLibraries%2FTag%20Editors%2FID3%20Tags#x9ba4c18
 
-eyeD3 --to-v2.4 --set-encoding=utf8 --artist="${ARTIST}" --album="${TITLE_BASE}" --title="${FULL_TYTLE}" --year=${MAKE_YEAR} --publisher=publisher --comment=jp:abc:"${TITLE_COMMENT}" --add-image=./agileradio.jpg:OTHER:agileradio.jpg  --set-user-url-frame="Description:${SITE_URL}" --set-user-text-frame="Description:${ARTIST}" --genre=radio --set-text-frame=TPE2:"${A_ARTIST}" --set-text-frame=TCOM:"${COMMPOSER}" --set-text-frame=TDAT:"${MAKE_YEAR}" --set-text-frame=TCOP:"${A_ARTIST}" --set-text-frame=TENC:lame ${RESULT_FILE}
+eyeD3 --to-v2.4 --set-encoding=utf8 --artist="${ARTIST}" --album="${TITLE_BASE}" --title="${FULL_TYTLE}" --year=${MAKE_YEAR} --publisher=agileradio --comment=jp:abc:"${TITLE_COMMENT}" --add-image=./agileradio.jpg:OTHER:agileradio.jpg  --set-user-url-frame="Description:${SITE_URL}" --set-user-text-frame="Description:${ARTIST}" --set-text-frame=TPE2:"${A_ARTIST}" --set-text-frame=TCOM:"${COMMPOSER}" --set-text-frame=TDAT:"${MAKE_YEAR}" --set-text-frame=TCOP:"${A_ARTIST}" --set-text-frame=TENC:lame ${RESULT_FILE}
 
 # 仕上げ。リネーム
 mv "${RESULT_FILE}" "${NAME_BASE}_ite${ITE_NO}.mp3"
